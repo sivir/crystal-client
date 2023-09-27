0dd14563244cace6ba7666ebc3f188b0112d6ebf
Jeffrey <jeffrey.magmachu@gmail.com>
2023-09-27 11:50:59 -0700

    starting search by name, moved file

--- supabase/functions/get-user/index.ts
+++ supabase/functions/get-user/index.ts
@@ -1,8 +1,10 @@
 // using deno since supabase uses deno
 import { serve } from 'https://deno.land/std@0.177.0/http/server.ts'
-import { update_db, cors_headers, get_user } from '../../shared/update_db.ts';
+import { update_db, cors_headers, get_user } from '../shared/update_db.ts';
 
 const riot_api_key = Deno.env.get('RIOT_API_KEY')!
+
+const region = "na1"; // will change
 
 serve(async (req) => {
 	if (req.method === 'OPTIONS') {
@@ -10,13 +12,16 @@ serve(async (req) => {
 	};
 
 	const x = await req.json();
-	const { id } = x;
+	const { summoner_name } = x;
 
 	try {
+		const summoner_response = await fetch(`https://${region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summoner_name}?api_key=${riot_api_key}`);
+		const summoner_data = await summoner_response.json();
+		const id = summoner_data.puuid;
+		console.log(id, "id");
 		const res = await get_user(id);
-		console.log("res", res);
 		if (res.length === 0) {
-			const response = await fetch(`https://na1.api.riotgames.com/lol/challenges/v1/player-data/${id}?api_key=${riot_api_key}`);
+			const response = await fetch(`https://${region}.api.riotgames.com/lol/challenges/v1/player-data/${id}?api_key=${riot_api_key}`);
 			const data = await response.json();
 			console.log("data", data);
 			await update_db(id, data);
